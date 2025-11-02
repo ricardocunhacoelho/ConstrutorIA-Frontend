8578be64908eee87473f9840b56b97fb74a43df6
RicardoCoelho <ricardo.coelho@shiftsistemas.com.br>
2025-11-02 09:19:53 -0300

    work
    work

--- src/app/layout/sidebar-menu.component.ts
+++ src/app/layout/sidebar-menu.component.ts
@@ -47,7 +47,7 @@ export class SidebarMenuComponent extends AppComponentBase implements OnInit {
             new MenuItem(this.l('HomePage'), '/app/home', 'fas fa-home'),
             new MenuItem(this.l('Obras'), '/app/obras', 'fas fa-building'),
             new MenuItem(this.l('Tarefas Obras'), '/app/tarefas', 'fas fa-tasks'),
-            new MenuItem(this.l('Tarefas Escritório'), '/app/escritorio-tarefas', 'fa-solid fa-stapler'),
+            new MenuItem(this.l('Tarefas Internas'), '/app/escritorio-tarefas', 'fa-solid fa-stapler'),
             new MenuItem(this.l('Solicitações Materiais'), '/app/solicitacoes-materiais', 'fa fa-shopping-bag'),
             new MenuItem(this.l('Problemas/Impedimentos'), '/app/problemas-impedimentos', 'fa fa-exclamation-triangle'),
             new MenuItem(this.l('Roles'), '/app/roles', 'fas fa-theater-masks', 'Pages.Roles'),
